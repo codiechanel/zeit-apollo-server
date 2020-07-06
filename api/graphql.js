551c8658a2ce999390ed1946d13eddbf2f6f2adc
codiechanel <codiechanel@gmail.com>
2020-07-06 10:35:55 +0800

    hj

--- api/graphql.js
+++ api/graphql.js
@@ -1,24 +1,30 @@
 // import { news, rss, suggest } from "./_resolvers";
 
-const { ApolloServer } = require("apollo-server-micro");
+const { ApolloServer } = require('apollo-server-micro')
 
-const cors = require("micro-cors")();
+const cors = require('micro-cors')()
 
 // const typeDefs = require('./_typeDefs')
 // const resolvers = require("./_resolvers");
-import { merge } from "lodash";
-import { typeDef as Pocket, resolvers as pocketResolvers } from "./_Pocket";
-import { typeDef as Npm, resolvers as npmResolvers } from "./_Npm";
-import { typeDef as Google, resolvers as googleResolvers } from "./_Google";
+import { merge } from 'lodash'
+import {
+  typeDef as Pocket,
+  resolvers as pocketResolvers,
+} from './_queries/_Pocket'
+import { typeDef as Npm, resolvers as npmResolvers } from './_queries/_Npm'
+import {
+  typeDef as Google,
+  resolvers as googleResolvers,
+} from './_queries/_Google'
 import {
   typeDef as Raindrop,
   resolvers as raindropResolvers,
-} from "./_Raindrop";
+} from './_queries/_Raindrop'
 import {
   typeDef as Crunchbase,
   resolvers as crunchbaseResolvers,
-} from "./_Crunchbase";
-import { gql } from "apollo-server-micro";
+} from './_queries/_Crunchbase'
+import { gql } from 'apollo-server-micro'
 // import { typeDef as Query } from "./_Query";
 
 const Query = gql`
@@ -26,10 +32,10 @@ const Query = gql`
   type Query {
     _empty: String
   }
-`;
-const resolvers = {};
+`
+const resolvers = {}
 
-let typeDefs = [Query, Pocket, Raindrop, Crunchbase, Npm, Google];
+let typeDefs = [Query, Pocket, Raindrop, Crunchbase, Npm, Google]
 
 const server = new ApolloServer({
   typeDefs,
@@ -43,28 +49,28 @@ const server = new ApolloServer({
   ),
   introspection: true,
   playground: true,
-});
+})
 
 exports.config = {
   api: {
     bodyParser: false,
   },
-};
+}
 
 // module.exports
 
 const myHandler = (req, res, ...args) => {
-  if (req.method === "OPTIONS") return res.status(200).send();
+  if (req.method === 'OPTIONS') return res.status(200).send()
 
-  const handler = server.createHandler();
+  const handler = server.createHandler()
 
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "*");
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader('Access-Control-Allow-Origin', '*')
 
-  return handler(req, res, ...args);
-};
-module.exports = cors(myHandler);
+  return handler(req, res, ...args)
+}
+module.exports = cors(myHandler)
 // module.exports = cors(server.createHandler());
